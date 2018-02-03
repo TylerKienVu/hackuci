00344489c43ac66b51a77027f6863cf1a420dfc3
Tyler Vu <tylerkienvu@gmail.com>
2018-02-03 06:01:18 -0800

    Basic core logic implemented
    - Added some core logic
    - Added some EJS view templates
    
    Problems:
    - Can't find way to pass result to view

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express"),
   bodyParser = require("body-parser"),
   Twit = require("twit");
 
+
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,19 +18,58 @@ var T = new Twit({
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 });
 
-T.get("search/tweets", { q: "#banana ", count: 4 }, function(
-  err,
-  data,
-  response
-) {
-  // console.log(data.statuses[0].text);
-  console.log(data);
-});
+/*------------Core Function------------*/
+
+/*Helper-Functions*/
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+/*----------------*/
+
+var hashtagArray = [{ q: "#banana", count: 100},{ q: "#apple", count: 100},
+{ q: "#orange", count: 100},{ q: "#peach", count: 100}];
+
+function grabTweets() {
+	var randomInt = getRandomInt(0,hashtagArray.length-1);
+	var hashtag = hashtagArray[randomInt].q;
+	T.get("search/tweets", hashtagArray[randomInt], function(
+	  err,
+	  data,
+	  response
+	) 
+	{	  
+	  var tweetArray = data.statuses;
+	  var textResultArray  = [];
+
+	  //loops until either it finds 4 valid results or reaches end of the tweet array
+	  for (i=0; textResultArray.length != 4 && i < tweetArray.length; i++) {
+	  	// console.log("Tweet[" + i + "] : " + tweetArray[i].text);
+	  	if (tweetArray[i].text.search(hashtag) != -1){
+			textResultArray.push(tweetArray[i].text.replace(
+	  			hashtag,"#GUESS-THE-HASHTAG"));  
+	  	}
+	  }
+
+	  if (textResultArray.length != 4){
+	  	console.log("Didn't get 4 valid results..");
+	  }
+
+	  // console.log(textResultArray);
+	  // console.log(hashtag);
+	  // console.log(result);
+	  var result = {result: textResultArray, hashtag: hashtag};
+	  // !!!!! then pass result to web page somehow !!!!!
+	});
+}
+grabTweets();
+
+/*------------------------------------*/
 
 app.get("/", function(req, res) {
-  res.send("This worked");
+  	res.render("pages/index");
 });
 
-// app.listen(3000, function() {
-//   console.log("Server has Started!");
-// });
+
+app.listen(3000, function() {
+  console.log("Server has Started!");
+});
