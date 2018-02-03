2b5b2a0950f33866725328d4cc22b8c4c84a444a
gevo99g <gevo99g@users.noreply.github.com>
2018-02-03 02:50:19 -0800

    got twitter working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express"),
-  bodyParser = require("body-parser"), 
+  bodyParser = require("body-parser"),
+  Twit = require("twit");
 
 var app = express();
 
@@ -7,10 +8,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+var T = new Twit({
+  consumer_key: "0eCmgHUU74uD0hMM3zRRABvG3",
+  consumer_secret: "As07P8NYUK2lGSLGm7xL0B27v7aFqKwq0XHr2CULBBQ6HSi6bU",
+  access_token: "959720804064813056-QKFCzftg275ZQosakTyAYHxvyTs8lJh",
+  access_token_secret: "ns4qkDijmJOfmJXkNjdYHJKZ8RP9zYoJJCAKtdxNym1Z2",
+  timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+});
+
+T.get("search/tweets", { q: "#banana ", count: 4 }, function(
+  err,
+  data,
+  response
+) {
+  // console.log(data.statuses[0].text);
+  console.log(data);
+});
+
 app.get("/", function(req, res) {
   res.send("This worked");
 });
 
-app.listen(3000, function() {
-  console.log("Server has Started!");
-});
+// app.listen(3000, function() {
+//   console.log("Server has Started!");
+// });
